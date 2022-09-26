d46d6950c7dbba7f73b8a6b1f9b70d67e494bba5
kenji.iwamoto <>
2022-09-26 12:53:36 +0900

    ユーザー取得処理

--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"fmt"
 	"todo_app/app/models"
-
 )
 
 func main() {
 	fmt.Println(models.Db)
-	u :=&models.User{}
 
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.Name = "test"
-
-	u.CreateUser()
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 }
