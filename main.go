2b8d05711536b9f1082cdb0c09406c5e53f8b729
kenji.iwamoto <>
2022-09-26 12:58:57 +0900

    ユーザー更新処理

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"todo_app/app/models"
+
 )
 
 func main() {
 	fmt.Println(models.Db)
-
 	u, _ := models.GetUser(1)
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	
 	fmt.Println(u)
 }
